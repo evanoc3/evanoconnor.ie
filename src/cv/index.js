dff0308039578186c1e38d2a673f8f0f649321f6
Evan O'Connor <evanoc3@gmail.com>
2024-02-20 00:16:01 +0000

    Added manual toggling of light/dark theme

--- src/cv/index.js
+++ src/cv/index.js
@@ -33,13 +33,43 @@ function setCurrentJobDuration() {
 }
 
 
+/**
+ * Checks to see if the browser javascript engine supports CSS media query matching and whether it prefers light/dark theme.
+ * Returns the theme the browser prefers or "light" by default.
+ * @return {"light" | "dark"} 
+ */
+function getCurrentBrowserTheme() {
+  return (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
+}
+
+
+function onThemeButtonClicked() {
+  const currentBrowserTheme = getCurrentBrowserTheme();
+  const bodyHasThemeAttribute = document.body.dataset.theme !== undefined;
+  const themeAttribute = document.body.dataset.theme;
+  let newThemeAttribute = "dark";
+
+  if (!bodyHasThemeAttribute) {
+    newThemeAttribute = (currentBrowserTheme === "light") ? "dark" : "light";
+  }
+  else {
+    newThemeAttribute = (themeAttribute === "light") ? "dark" : "light";
+  }
+
+  document.body.dataset.theme = newThemeAttribute;
+}
+
+
 /* Add the duration in current job to label */
 document.addEventListener("DOMContentLoaded", () => {
   setCurrentJobDuration();
-});
 
+  // setup print button
+  document.getElementById("print-btn").classList.add("visible");
+  document.getElementById("print-btn").addEventListener("click", () => window.print());
 
-/* Set event handler for print button */
-document.getElementById("print_btn").addEventListener("click", () => {
-	window.print();
+  // setup theme button
+  document.getElementById("theme-btn").classList.add("visible");
+  document.getElementById("theme-btn").addEventListener("click", onThemeButtonClicked);
+  document.body.dataset.theme = getCurrentBrowserTheme();
 });
