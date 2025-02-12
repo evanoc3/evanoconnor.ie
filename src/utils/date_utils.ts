08d3a979787066bfccbdefa297a09d3336f9ec9f
Evan O'Connor <evanoc3@gmail.com>
2025-02-12 19:45:53 +0000

    Clear repo

--- src/utils/date_utils.ts
+++
@@ -1,5 +0,0 @@
-const MONTH_STRINGS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-export function shortMonth(date: Date): string {
-	return MONTH_STRINGS[date.getMonth()];
-}
