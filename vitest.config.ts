62f9b64d695fc40bab4946c11305eca3aaf27109
Evan O'Connor <evanoc3@gmail.com>
2025-03-25 00:46:20 +0000

    Add unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { getViteConfig } from "astro/config";
+
+
+export default getViteConfig({
+  test: {
+    include: [
+      "**/tests/unit/**/*.test.ts"
+    ],
+    environment: "happy-dom"
+  }
+});
