fb23cc9a5bf7ddec68e662b18ef9dce8be8d7eb9
Evan O'Connor <evanoc3@gmail.com>
2025-08-17 14:40:02 +0100

    Updated and added more e2e tests

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -2,6 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test.describe("index page", () => {
 
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/", { timeout: 5000 });
+  });
+
   test("has the correct page title", async ({ page }) => {
     const resp = (await page.goto("/", { timeout: 5000 }))!;
     expect(resp).not.toBeNull();
@@ -11,8 +15,6 @@ test.describe("index page", () => {
   });
 
   test("has the correct meta tags", async ({ page }) => {
-    await page.goto("/", { timeout: 5000 });
-
     const metaCharset = page.locator("meta[charset]");
     await expect(metaCharset).toHaveAttribute("charset", "UTF-8");
 
@@ -24,23 +26,30 @@ test.describe("index page", () => {
 
     const metaLastBuiltTime = page.locator("meta[name=\"x-last-built-time\"]");
     expect(await metaLastBuiltTime.getAttribute("content")).toMatch(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/);
+
+    const linkCanonical = page.locator("link[rel=\"canonical\"]");
+    if(await linkCanonical.count() === 1) {
+      await expect(linkCanonical).toHaveAttribute("href", expect.stringMatching(/\/$/));
+    } else {
+      expect(await linkCanonical.count()).toBe(0);
+    }
   });
 
   test("has links to other pages", async ({ page }) => {
-    await page.goto("/", { timeout: 5000 });
-
     const cvLink = page.getByRole("link", { name: "/cv" });
     await expect(cvLink).toBeVisible();
-    await expect(cvLink).toHaveAttribute("href", "/cv");
+    await expect(cvLink).toHaveAttribute("href", "/cv/");
 
     const lightspeedLink = page.getByRole("link", { name: "/experiments/lightspeed" });
     await expect(lightspeedLink).toBeVisible();
-    await expect(lightspeedLink).toHaveAttribute("href", "/experiments/lightspeed");
+    await expect(lightspeedLink).toHaveAttribute("href", "/experiments/lightspeed/");
+
+    const tableOfContentsLink = page.getByRole("link", { name: "/experiments/toc" });
+    await expect(tableOfContentsLink).toBeVisible();
+    await expect(tableOfContentsLink).toHaveAttribute("href", "/experiments/toc/");
   });
 
   test("footer is visible (with JS enabled)", async ({ page }) => {
-    await page.goto("/", { timeout: 5000 });
-
     const footer = page.locator("footer");
     await expect(footer).toBeVisible();
   });
@@ -49,8 +58,6 @@ test.describe("index page", () => {
     test.use({ javaScriptEnabled: false });
 
     test("footer is hidden", async ({ page }) => {
-      await page.goto("/", { timeout: 5000 });
-
       const footer = page.locator("footer");
       await expect(footer).toBeHidden();
     });
